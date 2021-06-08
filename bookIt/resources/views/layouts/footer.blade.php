a377c39e61671d57032655f5253b7732ed913197
Chel <chelzakaria20@gmail.com>
2021-06-08 21:43:21 +0100

    about

--- bookIt/resources/views/layouts/footer.blade.php
+++ bookIt/resources/views/layouts/footer.blade.php
@@ -1,11 +1,11 @@
-<div style="background: #000; width:100%;" class="pt-4 p-3">
+<div style="background: #000; width:100%; " class="pt-4 p-3">
     <div class="container ">
         <div class="row">
            <div class="col ">
             <nav class="nav flex-column " style="font-weight: 300;">
                 <span class="nav-link text-white " style="font-weight: 600;" href="#">Company</span>
                 <a class="nav-link text-white"  href="{{ route('home') }}">Home</a>
-                <a class="nav-link text-white" href="#">About</a>
+                <a class="nav-link text-white" href="{{ route('about') }}">About</a>
                 <a class="nav-link text-white" href="#">Pricing</a>
                 <a class="nav-link text-white" href="#">Contact</a>
                
