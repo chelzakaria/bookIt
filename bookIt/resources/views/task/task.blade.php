189e1b369c1578c78e82f5d729a9962735f1628e
othmanedf <83710908+othmanedf@users.noreply.github.com>
2021-06-18 21:03:12 +0100

    Update task.blade.php

--- bookIt/resources/views/task/task.blade.php
+++ bookIt/resources/views/task/task.blade.php
@@ -3,6 +3,7 @@
 
     <div class="container-fluid">
         <script>
+        
             function allowDrop(ev) {
               ev.preventDefault();
             }
@@ -19,6 +20,7 @@
               ev.target.appendChild(document.getElementById(data));
               console.log(document.getElementById(data))
             }
+
             </script>
         <div class="row">
             @include('notes.layouts.sidebar')
@@ -43,7 +45,9 @@
                                     <div class="col-md-12">
                                       
                                      <div class="card mb-5 d-flex flex-column " ondrop="drop(event)" ondragover="allowDrop(event)" style="width: 18rem; height:29rem;border-radius:10px;background-color:rgb(217, 222, 224) ">
-                                         
+                                       <!--test -->
+                                     
+                                       <!---->
                                        </div>
                                     </div>
                                  </div>
@@ -61,9 +65,9 @@
                                      <div class="card mb-5 d-flex flex-column" ondrop="drop(event)" ondragover="allowDrop(event)" style="width: 18rem; height:29rem;border-radius:10px;background-color:rgb(217, 222, 224) ">
                                         <!-- div 1 --> 
                                         
-                                        <div class="card mb-5 p-2 ml-3 mt-3 "   draggable="true"  ondragstart="drag(event)" id="drag1" style="width: 16rem; height:9rem;border-radius:10px;background-color:rgb(114, 189, 15) ">
-                                        <div class="card-body pb-0">
-                                           <span class="card-text " style="font-weight: 400;font-size:15px; 
+                                        <div class="draggable card mb-5 p-2 ml-3 mt-3 "   draggable="true"  ondragstart="drag(event)" id="drag1" style="width: 16rem; height:9rem;border-radius:10px;background-color:rgb(114, 189, 15) ">
+                                        <div class="card-body pb-0"  >
+                                           <span class="card-text "  style="font-weight: 400;font-size:15px; 
                                              height:4.2rem;     overflow: hidden;
                                                 display: -webkit-box;
                                                 -webkit-line-clamp: 3;
@@ -71,15 +75,15 @@
                                              ">
                                              hihpipi</span>  
                                               
-                                            <p class="text-muted float-right mb-0 mt-4" style="font-weight: 300;font-size:13px;">jj</p>
-                                            <p class="mb-0 mt-4 " style="font-weight: 700;font-size:12px;color:#353535">
+                                            <p  class="text-muted float-right mb-0 mt-4" style="font-weight: 300;font-size:13px;">jj</p>
+                                            <p  class="mb-0 mt-4 " style="font-weight: 700;font-size:12px;color:#353535">
                                             </p>
                                          </div>
                                         </div>
                                         
                                         <!-- -->
                                           <!-- div 2 --> 
-                                          <div class="card mb-5 p-2 ml-3 mt-3" name="n" draggable="true"  ondragstart="drag(event)" id="drag2" style="width: 16rem; height:9rem;border-radius:10px;background-color:rgb(174, 15, 189) ">
+                                          <div class="draggable card mb-5 p-2 ml-3 mt-3"  draggable="true"  ondragstart="drag(event)" id="drag2" style="width: 16rem; height:9rem;border-radius:10px;background-color:rgb(174, 15, 189) ">
                                             <div class="card-body pb-0">
                                                <span class="card-text " style="font-weight: 400;font-size:15px; 
                                                  height:4.2rem;     overflow: hidden;
