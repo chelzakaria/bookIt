bb1c0ab0d1c77ad98868e61ca4ce6d7be1d57225
othmanedf <83710908+othmanedf@users.noreply.github.com>
2021-06-20 20:19:46 +0100

    Update task.blade.php

--- bookIt/resources/views/task/task.blade.php
+++ bookIt/resources/views/task/task.blade.php
@@ -130,8 +130,8 @@ function unwrap(node) {
                         <!--task1-->
                         <div class="dropzone rounded " ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"> &nbsp; </div> 
                         <div class="draggable shadow-sm"></div>
+                    
                             @foreach ($tasks1 as $task)
-
                         <div class="card draggable shadow-sm" style="border-radius: 20px;background-color:rgb(255, 0, 0);" id="cd<?php echo $task->id?>" draggable="true" ondragstart="drag(event)">
                             <div class="card-body p-2">
                                 <div class="card-title" >
@@ -159,7 +159,9 @@ function unwrap(node) {
                     <div class="items " id="f">
                         <div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"> &nbsp; </div> 
                         <div class="draggable shadow-sm"></div>
+                        
                         @foreach ($tasks2 as $task)
+                        
                         <div class="card draggable shadow-sm" style="border-radius: 20px;background-color:yellow" id="cd<?php echo $task->id?>" draggable="true" ondragstart="drag(event)">
                             <div class="card-body p-2">
                                 <div class="card-title">
@@ -188,6 +190,7 @@ function unwrap(node) {
                         <div class="dropzone rounded " ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"> &nbsp; </div> 
                         <div class="draggable shadow-sm"></div>
                         @foreach ($tasks3 as $task)
+                        
                         <div class="card draggable shadow-sm" style="border-radius: 20px;background-color:green" id="cd<?php echo $task->id?>" draggable="true" ondragstart="drag(event)">
                           
                             <div class="card-body p-2">
