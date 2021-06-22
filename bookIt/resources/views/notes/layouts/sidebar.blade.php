13b40c52e1745ecce456b6442bef77f7de6a1e03
Chel <chelzakaria20@gmail.com>
2021-06-22 23:07:40 +0100

    sidebar

--- bookIt/resources/views/notes/layouts/sidebar.blade.php
+++ bookIt/resources/views/notes/layouts/sidebar.blade.php
@@ -7,8 +7,8 @@
        }
        </style>
        <div class="sticky-top">
-    <img  id="large_button"  class="d-none d-md-inline mb-4 mt-3"  src="/images/logos/logo_bottom.svg" alt="" style="width:75%; height:auto;">
-    <img   class=" d-none  d-sm-block  d-md-none d-block d-sm-none mb-4 mt-1"  src="/images/logos/logo_sidebar.svg" alt="" style="width:37px; height:auto;">
+    <img  id="large_button"  class="d-none d-md-inline mb-4 mt-3"  src="/images/logos/logo_bottom.svg" alt="" style="width:75%; height:auto;cursor: pointer;">
+    <img   class=" d-none  d-sm-block  d-md-none d-block d-sm-none mb-4 mt-1"  src="/images/logos/logo_sidebar.svg" alt="" style="width:37px; height:auto;cursor: pointer;">
         <div class="mb-4 mt-4 hv " >
             <a style="font-weight: 400; color:#fff; font-size:14px; text-decoration:none;" href="#"><img class="mr-3" src="/images/icons/dashboard_icon.svg" alt="" style="height:auto; width:26px;" > <span class="d-none d-md-inline">Dashboard</span> <span class="py-1 px-1 ml-3 badge badge-success font-weight-bold" >02</span></a>
         </div>
@@ -27,18 +27,18 @@
         <div class="mb-4 hv">
             <a   style="font-weight: 400; color:#fff; font-size:14px; text-decoration:none;" href="{{route('setting')}}"><img class="mr-3 " src="/images/icons/setting_icon.svg" alt="" style="height:auto; width:26px;" > <span class="d-none d-md-inline">Setting</span></a>
         </div>
-        <div class="fixed-bottom mb-3 ml-4" style="width: 20%">
+        <div class="fixed-bottom mb-3 ml-2" style="width: 20%">
              <form action="{{ route('logout') }}" method="POST" >
                 @csrf
                 <button type="submit" class="btn"  style="font-weight: 400; color:#fff; font-size:14px; text-decoration:none;" >
-                     <img class="mr-2 " src="/images/icons/logout_icon.svg" alt="" style="height:auto; width:30px;" > <span class="d-none d-md-inline">Log out</span> 
+                     <img class="mr-3 " src="/images/icons/logout_icon.svg" alt="" style="height:auto; width:30px;" > <span class="d-none d-md-inline">Log out</span> 
                 </button>
             </form>
         </div>
 </div>
 </div>
 
-<div id="small" class="col-1 py-4 pl-4 pr-0" style="min-height:100vh  ;background:#1F1A6B; display:none; ">
+<div id="small" class="col-1 py-4 pl-4 pr-0 " style="min-height:100vh  ;background:#1F1A6B; display:none; ">
     <style>
         .hv:hover{
          transform: scale(1.1);
@@ -66,11 +66,11 @@
          <div class="mb-4 hv">
              <a   style="font-weight: 400; color:#fff; font-size:14px; text-decoration:none;" href="{{route('setting')}}"><img class="mr-3 " src="/images/icons/setting_icon.svg" alt="" style="height:auto; width:26px;" >  </a>
          </div>
-         <div class="fixed-bottom mb-3 ml-4" style="width: 20%">
+         <div class="fixed-bottom mb-3 ml-2" style="width: 20%">
               <form action="{{ route('logout') }}" method="POST" >
                  @csrf
                  <button type="submit" class="btn"  style="font-weight: 400; color:#fff; font-size:14px; text-decoration:none;" >
-                      <img class="mr-2 " src="/images/icons/logout_icon.svg" alt="" style="height:auto; width:30px;" > 
+                      <img class="mr-3   " src="/images/icons/logout_icon.svg" alt="" style="height:auto; width:30px;" > 
                  </button>
              </form>
          </div>
